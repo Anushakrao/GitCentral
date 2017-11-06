3bd71f1ee66e331ac6a9a4fd7d8135040c083795
Anusha k <anukrao94@gmail.com>
2017-11-06 12:24:22 +0530

    commit

--- app/gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp'),
-  create = require('gulp-cordova-create'),
-  version = require('gulp-cordova-version');
-
-gulp.task('default', function () {
-  return gulp.src('')
-    .pipe(version(require('./package.json').version));
-});
